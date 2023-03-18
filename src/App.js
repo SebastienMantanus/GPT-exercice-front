3e52a42d575a96ffd22bce688afa9ca82a9f6699
Sébastien Mantanus <sebastienmantanus@iMac-de-Sebastien.local>
2023-03-18 12:58:10 +0100

    first test release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [response, setResponse] = useState();
+  const [question, setQuestion] = useState("");
+  const [onRequest, setOnRequest] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const responseData = async (request) => {
+      try {
+        setOnRequest(true);
+        const response = await axios.post(
+          "https://site--gpt-exercice--vqtmsgjlf7qx.code.run/chat/",
+          {
+            question: request,
+          }
+        );
+        setResponse(response.data.response);
+        setOnRequest(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    responseData(question);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Coucou ! bienvenue sur mon Chat GPT !</h1>
+      <div className="chatBox">
+        <form onSubmit={handleSubmit}>
+          <textarea
+            name="message"
+            cols="60"
+            rows="10"
+            placeholder="Votre message ici..."
+            value={question}
+            onChange={(event) => {
+              setQuestion(event.target.value);
+            }}
+          ></textarea>
+          {!onRequest ? (
+            <button>Poser votre question !</button>
+          ) : (
+            <p>En cours de traitement...</p>
+          )}
+        </form>
+        {response && (
+          <div className="ansewr">
+            <h2>Voici votre réponse...</h2>
+            <p>{response}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
